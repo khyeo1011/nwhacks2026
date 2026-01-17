8a7ce236f4b01b4cb43e9074172fb9ffba4a04ae
yood2 <yood2@proton.me>
2026-01-17 12:54:45 -0800

    frontend add lib files

---
+++ frontend/lib/api.ts
@@ -0,0 +1,36 @@
+function register(userId: string, password: string) {
+    return
+}
+
+function login(useId: string, password: string) {
+    return
+}
+
+export function getPendingQuests(userId: string) {
+    const sample = 
+        [
+            { questId: "1", prompt: "test", hostId: "testHost1" },
+            { questId: "2", prompt: "test", hostId: "testHost2" },
+            { questId: "3", prompt: "test", hostId: "testHost3" },
+            { questId: "4", prompt: "test", hostId: "testHost4" },
+            { questId: "5", prompt: "test", hostId: "testHost5" },
+        ]
+
+    return sample
+}
+
+function getCompletedQuests(userId: string) {
+    return
+}
+
+function createQuest(prompt: string, hostId: string, userIds: string[], image: any, time: number) {
+    return
+}
+
+function getPrompt() {
+    return
+}
+
+function completeQuest(questId: string, userId: string, image: any, time: number) {
+    return
+}
